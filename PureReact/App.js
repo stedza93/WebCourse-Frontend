0980157b2b9f31c14bc9e900aa1fd87dc2e495ef
stefan.miladinovic <stefanmila93@gmail.com>
2020-04-27 23:56:35 +0200

    pravljenje komponente Student i ubacivanje u komponentu App

--- PureReact/App.js
+++ PureReact/App.js
@@ -1,12 +1,20 @@
-   // console.log(React,ReactDOM) /*testiranje da li smo pravilno ukljucili biblioteke*/
-   const App = () => {
-    return React.createElement(
-      "div",
-      {},
-      [React.createElement("h1", {key:2}, "Student"),React.createElement("h1", {key:1}, "Student")]
-    );
-  };
-  ReactDOM.render(
-    React.createElement(App),
-    document.getElementById("root")
-  );
+// console.log(React,ReactDOM) /*testiranje da li smo pravilno ukljucili biblioteke*/
+
+const Student = () => {
+  return React.createElement("div", {}, [
+    React.createElement("h2", {}, "Marko"),
+    React.createElement("h2", {}, "Maja"),
+    React.createElement("h2", {}, "Mirko"),
+    React.createElement("hr")
+  ]);
+};
+
+const App = () => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", { key: 2 }, "Studenti:"),
+    React.createElement(Student),
+    React.createElement(Student),
+    React.createElement(Student)
+  ]);
+};
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
