fc5ef4589e68a167c96c1a064749145de97ac3c6
stefan.miladinovic <stefanmila93@gmail.com>
2020-04-28 00:04:49 +0200

    Prosledjivanje props Student komponenti

--- PureReact/App.js
+++ PureReact/App.js
@@ -1,20 +1,21 @@
 // console.log(React,ReactDOM) /*testiranje da li smo pravilno ukljucili biblioteke*/
 
-const Student = () => {
+const Student = (props) => {
+  console.log(props);
   return React.createElement("div", {}, [
     React.createElement("h2", {}, "Marko"),
     React.createElement("h2", {}, "Maja"),
     React.createElement("h2", {}, "Mirko"),
-    React.createElement("hr")
+    React.createElement("hr"),
   ]);
 };
 
 const App = () => {
   return React.createElement("div", {}, [
     React.createElement("h1", { key: 2 }, "Studenti:"),
-    React.createElement(Student),
-    React.createElement(Student),
-    React.createElement(Student)
+    React.createElement(Student, { name: "Marko", fakultet: "FPN" }),
+    React.createElement(Student, { name: "Maja", fakultet: "PMF" }),
+    React.createElement(Student, { name: "Mirko", fakultet: "ETF" }),
   ]);
 };
 ReactDOM.render(React.createElement(App), document.getElementById("root"));
