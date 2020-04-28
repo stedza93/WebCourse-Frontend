3211874dcf49bd8a73143c68dae62423bf0e5dd7
stefan.miladinovic <stefanmila93@gmail.com>
2020-04-28 03:24:11 +0200

    one component one file! Exportovanje Student komponente u odvojeni fajl i njen import u App.js

--- PureReact/App.js
+++ PureReact/App.js
@@ -1,23 +1,6 @@
 import React from "react";
 import { render } from "react-dom";
-// console.log(React,ReactDOM) /*testiranje da li smo pravilno ukljucili biblioteke*/
-
-const Student = (props) => {
-  console.log(props);
-  // return React.createElement("div", {}, [
-  //   React.createElement("h2", {}, props.name),
-  //   React.createElement("h5", {}, props.fakultet),
-  //   React.createElement("hr"),
-  // ]);
-  return (
-    <div>
-      <h2>{props.name}</h2>
-      <h3>{props.fakultet}</h3>
-      <hr></hr>
-    </div>
-  );
-};
-
+import { Student } from "./Student";
 const App = () => {
   // return React.createElement("div", {}, [
   //   React.createElement("h1", { key: 2 }, "Studenti:"),
