12e02685573583d32abc043fd0bf8624d105bb33
stefan.miladinovic <stefanmila93@gmail.com>
2020-04-28 03:26:29 +0200

    defult export za komponentu Student

--- PureReact/App.js
+++ PureReact/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { render } from "react-dom";
-import { Student } from "./Student";
+import Student from "./Student";
 const App = () => {
   // return React.createElement("div", {}, [
   //   React.createElement("h1", { key: 2 }, "Studenti:"),
@@ -8,13 +8,13 @@ const App = () => {
   //   React.createElement(Student, { name: "Maja", fakultet: "Maja" }),
   //   React.createElement(Student, { name: "Mirko", fakultet: "ETF" }),
   // ]);
-  return(
+  return (
     <div>
       <h1>Studenti:</h1>
       <Student name="Marko" fakultet="FPN"></Student>
       <Student name="Maja" fakultet="PMF"></Student>
       <Student name="Mirko" fakultet="ETF"></Student>
     </div>
-  )
+  );
 };
 render(React.createElement(App), document.getElementById("root"));
--- PureReact/Student.js
+++ PureReact/Student.js
@@ -1,15 +1,17 @@
 import React from "react";
 // console.log(React,ReactDOM) /*testiranje da li smo pravilno ukljucili biblioteke*/
-export const Student = (props) => {
+export default function Student(props) {
   console.log(props);
   // return React.createElement("div", {}, [
   //   React.createElement("h2", {}, props.name),
   //   React.createElement("h5", {}, props.fakultet),
   //   React.createElement("hr"),
   // ]);
-  return (<div>
-    <h2>{props.name}</h2>
-    <h3>{props.fakultet}</h3>
-    <hr></hr>
-  </div>);
-};
+  return (
+    <div>
+      <h2>{props.name}</h2>
+      <h3>{props.fakultet}</h3>
+      <hr></hr>
+    </div>
+  );
+}
