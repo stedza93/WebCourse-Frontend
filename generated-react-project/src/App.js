ea491f4e870e48a950ed3b5a0581abe6b7189f69
stefan.miladinovic <stefanmila93@gmail.com>
2020-04-28 03:35:29 +0200

    izmijenjen sadrzaj App.js fajla

--- generated-react-project/src/App.js
+++ generated-react-project/src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Prva generisana React aplikacija
         </a>
       </header>
     </div>
