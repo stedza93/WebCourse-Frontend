a238de992b079c6fdbc6ddad0920e17f68a4208c
stefan.miladinovic <stefan@abago.com>
2020-05-11 03:39:47 +0200

    dodata fja sendToUserPage unutar User komponente koja salje na stranicu user profila

--- user-app/src/User.js
+++ user-app/src/User.js
@@ -1,7 +1,10 @@
 import React from "react";
 export function User(props) {
+  function sendToUserPage() {
+    window.open(props.userInfo.html_url, "_blank");
+  }
   return (
-    <div className="card">
+    <div className="card" onClick={sendToUserPage}>
       <div className="image">
         <img src={props.userInfo.avatar_url} />
       </div>
@@ -10,12 +13,12 @@ export function User(props) {
         <div className="meta">
           <a>Username: {props.userInfo.login}</a>
         </div>
-        <div className="description">
-        {props.userInfo.bio}
-        </div>
+        <div className="description">{props.userInfo.bio}</div>
       </div>
       <div className="extra content">
-        <span className="right floated">Repos: {props.userInfo.public_repos}</span>
+        <span className="right floated">
+          Repos: {props.userInfo.public_repos}
+        </span>
         <span>
           <i className="user icon"></i>
           {props.userInfo.followers}
