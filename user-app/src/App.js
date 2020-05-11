cb3cb256e2be16f54401b0865cbc4ef9a35d3f63
stefan.miladinovic <stefan@abago.com>
2020-05-11 02:15:25 +0200

    Prebacivanje CSS stilova u varijablu

--- user-app/src/App.js
+++ user-app/src/App.js
@@ -5,16 +5,15 @@ function App() {
   const text = "Hello world!",
     myNumber = 123,
     myArray = [1, 2, 3],
-    myObject = { name: "John", job: "Developer" };
+    myObject = { name: "John", job: "Developer" },
+    containerStyle = {
+      background: "red",
+      marginTop: "20px",
+      padding: "20px",
+      color: "#fff",
+    };
   return (
-    <div
-      style={{
-        background: "red",
-        marginTop: "20px",
-        padding: "20px",
-        color: "#fff",
-      }}
-    >
+    <div style={containerStyle}>
       <ul>
         <li>{text}</li>
         <li>{myNumber}</li>
