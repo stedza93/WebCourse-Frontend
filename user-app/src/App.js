ad978d278c594d964314d8950097e9e3fca624fe
stefan.miladinovic <stefan@abago.com>
2020-05-11 02:29:46 +0200

    Pretvaranje koda koji ponavljamo u komponentu

--- user-app/src/App.js
+++ user-app/src/App.js
@@ -1,17 +1,41 @@
 import React from "react";
 import "./App.css";
-
+function User() {
+  return (
+    <div className="card">
+      <div className="image">
+        <img src="https://semantic-ui.com/images/avatar2/large/matthew.png" />
+      </div>
+      <div className="content">
+        <div className="header">Matt Giampietro</div>
+        <div className="meta">
+          <a>Friends</a>
+        </div>
+        <div className="description">
+          Matthew is an interior designer living in New York.
+        </div>
+      </div>
+      <div className="extra content">
+        <span className="right floated">Joined in 2013</span>
+        <span>
+          <i className="user icon"></i>
+          75 Friends
+        </span>
+      </div>
+    </div>
+  );
+}
 function App() {
-  const text = "Hello world!",
-    myNumber = 123,
-    myArray = [1, 2, 3],
-    myObject = { name: "John", job: "Developer" },
-    containerStyle = {
-      background: "red",
-      marginTop: "20px",
-      padding: "20px",
-      color: "#fff",
-    };
+  // const text = "Hello world!",
+  //   myNumber = 123,
+  //   myArray = [1, 2, 3],
+  //   myObject = { name: "John", job: "Developer" },
+  //   containerStyle = {
+  //     background: "red",
+  //     marginTop: "20px",
+  //     padding: "20px",
+  //     color: "#fff",
+  //   };
   return (
     // <div style={containerStyle} className="container-wrapper">
     //   <ul>
@@ -29,66 +53,9 @@ function App() {
     // </div>
     <div>
       <div className="ui link cards">
-        <div className="card">
-          <div className="image">
-            <img src="https://semantic-ui.com/images/avatar2/large/matthew.png"/></div>
-            <div className="content">
-              <div className="header">Matt Giampietro</div>
-              <div className="meta">
-                <a>Friends</a>
-              </div>
-              <div className="description">
-                Matthew is an interior designer living in New York.
-              </div>
-            </div>
-            <div className="extra content">
-              <span className="right floated">Joined in 2013</span>
-              <span>
-                <i className="user icon"></i>
-                75 Friends
-              </span>
-            </div>
-          </div>
-          <div className="card">
-          <div className="image">
-            <img src="https://semantic-ui.com/images/avatar2/large/matthew.png"/></div>
-            <div className="content">
-              <div className="header">Matt Giampietro</div>
-              <div className="meta">
-                <a>Friends</a>
-              </div>
-              <div className="description">
-                Matthew is an interior designer living in New York.
-              </div>
-            </div>
-            <div className="extra content">
-              <span className="right floated">Joined in 2013</span>
-              <span>
-                <i className="user icon"></i>
-                75 Friends
-              </span>
-            </div>
-          </div>
-          <div className="card">
-          <div className="image">
-            <img src="https://semantic-ui.com/images/avatar2/large/matthew.png"/></div>
-            <div className="content">
-              <div className="header">Matt Giampietro</div>
-              <div className="meta">
-                <a>Friends</a>
-              </div>
-              <div className="description">
-                Matthew is an interior designer living in New York.
-              </div>
-            </div>
-            <div className="extra content">
-              <span className="right floated">Joined in 2013</span>
-              <span>
-                <i className="user icon"></i>
-                75 Friends
-              </span>
-            </div>
-          </div>
+        <User></User>
+        <User></User>
+        <User></User>
       </div>
     </div>
   );
