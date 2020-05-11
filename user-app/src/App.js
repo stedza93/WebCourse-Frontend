739692feb5d9799cff77ae3b6950ece4762327c9
stefan.miladinovic <stefan@abago.com>
2020-05-11 02:09:45 +0200

    referenciranje varijabli unutar JSX-a

--- user-app/src/App.js
+++ user-app/src/App.js
@@ -3,7 +3,23 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <div>Hello</div>;
+  const text = "Hello world!",
+    myNumber = 123,
+    myArray = [1, 2, 3],
+    myObject = { name: "John", job: "Developer" };
+  return (
+    <div>
+      <ul>
+        <li>{text}</li>
+        <li>{myNumber}</li>
+        <li>{myArray}</li>
+        {/* <li>{myObject}</li> */}
+        <li>
+          Name: {myObject.name}, Job: {myObject.job}
+        </li>
+      </ul>
+    </div>
+  );
 }
 
 export default App;
