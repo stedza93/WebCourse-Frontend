8394378abb87b2b0b2b36f8bb5d6333a2fa80b0c
stefan.miladinovic <stefan@abago.com>
2020-05-11 02:40:09 +0200

    prevodjenje App komponente iz funkcionalne u class komponentu

--- user-app/src/App.js
+++ user-app/src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./App.css";
 import { User } from "./User";
-function App() {
+class App extends React.Component {
   // const text = "Hello world!",
   //   myNumber = 123,
   //   myArray = [1, 2, 3],
@@ -12,27 +12,35 @@ function App() {
   //     padding: "20px",
   //     color: "#fff",
   //   };
-  return (
-    // <div style={containerStyle} className="container-wrapper">
-    //   <ul>
-    //     <li>{text}</li>
-    //     <li>{myNumber}</li>
-    //     <li>{myArray}</li>
-    //     {/* <li>{myObject}</li>  greska, ne moze prikazati objekat*/}
-    //     <li>
-    //       Name: {myObject.name}, Job: {myObject.job}
-    //     </li>
-    //     {myNumber === 123 ? "Tacan broj" : "Pogresan broj"}
-    //   </ul>
-    //   <label htmlFor="name">Name:</label> <br></br>
-    //   <input type="text" id="name"></input>
-    // </div>
-    <div>
-      <div className="ui link cards">
-        <User userInfo={{name:"John", job:"Developer", bio:"Matthew is an interior designer living in New York."}}></User>
+  render() {
+    return (
+      // <div style={containerStyle} className="container-wrapper">
+      //   <ul>
+      //     <li>{text}</li>
+      //     <li>{myNumber}</li>
+      //     <li>{myArray}</li>
+      //     {/* <li>{myObject}</li>  greska, ne moze prikazati objekat*/}
+      //     <li>
+      //       Name: {myObject.name}, Job: {myObject.job}
+      //     </li>
+      //     {myNumber === 123 ? "Tacan broj" : "Pogresan broj"}
+      //   </ul>
+      //   <label htmlFor="name">Name:</label> <br></br>
+      //   <input type="text" id="name"></input>
+      // </div>
+      <div>
+        <div className="ui link cards">
+          <User
+            userInfo={{
+              name: "John",
+              job: "Developer",
+              bio: "Matthew is an interior designer living in New York.",
+            }}
+          ></User>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
