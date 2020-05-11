f3f699ef15014bbc0bba91b84cea74017056f480
stefan.miladinovic <stefan@abago.com>
2020-05-11 02:25:45 +0200

    Dodavanje kartica iz frameworka

--- user-app/src/App.js
+++ user-app/src/App.js
@@ -13,19 +13,83 @@ function App() {
       color: "#fff",
     };
   return (
-    <div style={containerStyle} className="container-wrapper">
-      <ul>
-        <li>{text}</li>
-        <li>{myNumber}</li>
-        <li>{myArray}</li>
-        {/* <li>{myObject}</li>  greska, ne moze prikazati objekat*/}
-        <li>
-          Name: {myObject.name}, Job: {myObject.job}
-        </li>
-        {myNumber === 123 ? "Tacan broj" : "Pogresan broj"}
-      </ul>
-      <label htmlFor="name">Name:</label> <br></br>
-      <input type="text" id="name"></input>
+    // <div style={containerStyle} className="container-wrapper">
+    //   <ul>
+    //     <li>{text}</li>
+    //     <li>{myNumber}</li>
+    //     <li>{myArray}</li>
+    //     {/* <li>{myObject}</li>  greska, ne moze prikazati objekat*/}
+    //     <li>
+    //       Name: {myObject.name}, Job: {myObject.job}
+    //     </li>
+    //     {myNumber === 123 ? "Tacan broj" : "Pogresan broj"}
+    //   </ul>
+    //   <label htmlFor="name">Name:</label> <br></br>
+    //   <input type="text" id="name"></input>
+    // </div>
+    <div>
+      <div className="ui link cards">
+        <div className="card">
+          <div className="image">
+            <img src="https://semantic-ui.com/images/avatar2/large/matthew.png"/></div>
+            <div className="content">
+              <div className="header">Matt Giampietro</div>
+              <div className="meta">
+                <a>Friends</a>
+              </div>
+              <div className="description">
+                Matthew is an interior designer living in New York.
+              </div>
+            </div>
+            <div className="extra content">
+              <span className="right floated">Joined in 2013</span>
+              <span>
+                <i className="user icon"></i>
+                75 Friends
+              </span>
+            </div>
+          </div>
+          <div className="card">
+          <div className="image">
+            <img src="https://semantic-ui.com/images/avatar2/large/matthew.png"/></div>
+            <div className="content">
+              <div className="header">Matt Giampietro</div>
+              <div className="meta">
+                <a>Friends</a>
+              </div>
+              <div className="description">
+                Matthew is an interior designer living in New York.
+              </div>
+            </div>
+            <div className="extra content">
+              <span className="right floated">Joined in 2013</span>
+              <span>
+                <i className="user icon"></i>
+                75 Friends
+              </span>
+            </div>
+          </div>
+          <div className="card">
+          <div className="image">
+            <img src="https://semantic-ui.com/images/avatar2/large/matthew.png"/></div>
+            <div className="content">
+              <div className="header">Matt Giampietro</div>
+              <div className="meta">
+                <a>Friends</a>
+              </div>
+              <div className="description">
+                Matthew is an interior designer living in New York.
+              </div>
+            </div>
+            <div className="extra content">
+              <span className="right floated">Joined in 2013</span>
+              <span>
+                <i className="user icon"></i>
+                75 Friends
+              </span>
+            </div>
+          </div>
+      </div>
     </div>
   );
 }
