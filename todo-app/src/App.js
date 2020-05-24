7b8eab9041329e9aebea0eaba781af130d17df5b
stefan.miladinovic <stefanmila93@gmail.com>
2020-05-24 04:36:34 +0200

    Dodata funkcionalnost dodavanja novog todo item-a

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -9,10 +9,27 @@ class App extends React.Component {
       { id: 2, content: "Prosetaj psa" },
     ],
   };
+  addTodo = (e) => {
+    e.preventDefault();
+    let newTodos = this.state.todos;
+    let newElement = {
+      id: this.state.todos.length + 2,
+      content: this.state.todoName,
+    };
+    newTodos.push(newElement);
+    this.setState({ todos: newTodos });
+  };
   render() {
     return (
       <div className="container">
         <h1 className="center">Todo List</h1>
+        <form onSubmit={this.addTodo}>
+          <input
+            type="text"
+            value={this.state.todoName}
+            onChange={(e) => this.setState({ todoName: e.target.value })}
+          ></input>
+        </form>
         <Todos todos={this.state.todos}></Todos>
       </div>
     );
