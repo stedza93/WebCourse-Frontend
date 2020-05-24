29054552af5be9526751b7ab0d9cc071b65aa22b
stefan.miladinovic <stefanmila93@gmail.com>
2020-05-24 04:33:11 +0200

    Sadrzaj Todos komponente vrijednost todoList promjenljive

--- todo-app/src/Todos.js
+++ todo-app/src/Todos.js
@@ -1,21 +1,19 @@
-import React from 'react';
-const Todos = ({todos}) => {
-   console.log(todos)
-    const todoList = todos.length ? (
-        todos.map((todo) => {
-          return (
-            <div className="collection-item" key={todo.id}>
-              <span>{todo.content}</span>
-              <input type="checkbox" checked={todo.checked}></input>
-            </div>
-          );
-        })
-      ) : (
-        <p className="center">Svi taskovi su zavrseni</p>
+import React from "react";
+const Todos = ({ todos }) => {
+  console.log(todos);
+  const todoList = todos.length ? (
+    todos.map((todo) => {
+      return (
+        <div className="collection-item" key={todo.id}>
+          <span>{todo.content}</span>
+          <input type="checkbox" checked={todo.checked}></input>
+        </div>
       );
-    return ( <div className="collection">
-      <div className="collection-item">Alvin</div>
-    </div> );
-}
- 
+    })
+  ) : (
+    <p className="center">Svi taskovi su zavrseni</p>
+  );
+  return <div className="collection">{todoList}</div>;
+};
+
 export default Todos;
