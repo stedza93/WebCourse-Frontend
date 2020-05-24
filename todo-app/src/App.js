a6e2d0ac97d1822e2b765a1f004abfe7e3261070
stefan.miladinovic <stefanmila93@gmail.com>
2020-05-24 04:20:27 +0200

    App komponenta prebacena u class komponentu i inicijalizovan state

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,10 +1,16 @@
-import React from 'react';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    todoName: "",
+    todos: [
+      { id: 1, content: "Uradi vjezbe" },
+      { id: 2, content: "Prosetaj psa" },
+    ],
+  };
+  render() {
+    return <div className="App"></div>;
+  }
 }
 
 export default App;
