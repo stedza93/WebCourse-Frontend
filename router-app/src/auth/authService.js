05cf4214e8eb40bcf48b998a9c02895d5dafd530
stefan.miladinovic <stefanmila93@gmail.com>
2020-06-08 07:55:12 +0200

    promijenjena logika za verifikaciju statusa, i dodata funkcija za setovanje tokena u localStorage

--- router-app/src/auth/authService.js
+++ router-app/src/auth/authService.js
@@ -1,6 +1,13 @@
+import apiCall from "../services/apiCall";
 class AuthService {
-    isAuthenticated = false;
-    getAuthStatus() {return this.isAuthenticated; }
+    getAuthStatus() {
+      let token = localStorage.getItem("token");
+      if(!!token)this.setJwt(token)
+      return !!token ? true : false;
+    }
+    setJwt(token) {
+      apiCall.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+    }
     login() {
       this.isAuthenticated = true;
       alert('Ulogovan')
