9ed8e1527fe314eac5186d8d2a6b8df17b316927
stefan.miladinovic <stefanmila93@gmail.com>
2020-06-08 08:02:31 +0200

    dodat inteceptor za 401 error

--- router-app/src/services/apiCall.js
+++ router-app/src/services/apiCall.js
@@ -1,9 +1,19 @@
 import axios from "axios";
+import {auth} from "../auth/authService"
 const apiUrl = "http://localhost:3001/";
 
 const instance= axios.create({
   baseURL: apiUrl
 });
-
+instance.interceptors.response.use(function (response) {
+    return response;
+  }, function (error) {
+    if (401 === error.response.status) {
+        console.log(error)
+      auth.logout()
+    } else {
+        return Promise.reject(error);
+    }
+  });
 
 export default instance;
