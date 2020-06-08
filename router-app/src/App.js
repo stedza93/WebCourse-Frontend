76ae5374e3b93bf1087d1cd3b76e9dfa330d825c
stefan.miladinovic <stefanmila93@gmail.com>
2020-06-08 04:38:27 +0200

    Switch dodat i 404 handler

--- router-app/src/App.js
+++ router-app/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Route, Link } from "react-router-dom";
+import { BrowserRouter, Route, Link, Switch } from "react-router-dom";
 import "./App.css";
 import { Users } from "./components/Users";
 
@@ -7,8 +7,11 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
-     <BrowserRouter>
+        <BrowserRouter>
+        <Switch>
           <Route path="/(users|user|accounts)/" component={Users} />
+          <Route path="*" render={()=><h1>404 Not Found</h1>}></Route>
+          </Switch>
         </BrowserRouter>
       </div>
     );
