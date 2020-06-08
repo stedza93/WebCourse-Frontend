88756e035777010197f168b8089bddaf413d9a3a
stefan.miladinovic <stefanmila93@gmail.com>
2020-06-08 04:19:16 +0200

    putanja rute kao regularni izraz

--- router-app/src/App.js
+++ router-app/src/App.js
@@ -7,7 +7,9 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <Users></Users>
+     <BrowserRouter>
+          <Route path="/(users|user|accounts)/" component={Users} />
+        </BrowserRouter>
       </div>
     );
   }
