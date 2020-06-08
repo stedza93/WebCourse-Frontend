e2a346c5524be045932c967677be03572e81ffa7
stefan.miladinovic <stefanmila93@gmail.com>
2020-06-08 04:15:15 +0200

    App prevedena u class komponentu

--- router-app/src/App.js
+++ router-app/src/App.js
@@ -1,14 +1,16 @@
 import React from "react";
-
+import { BrowserRouter, Route, Link } from "react-router-dom";
 import "./App.css";
 import { Users } from "./components/Users";
 
-function App() {
-  return (
-    <div className="App">
-      <Users></Users>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <Users></Users>
+      </div>
+    );
+  }
 }
 
 export default App;
