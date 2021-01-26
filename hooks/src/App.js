839bed9d59fb7ae963cf5f1417250fac400623a7
Stefan Miladinovic <stefan@coinis.com>
2021-01-26 05:52:58 +0100

    initalState sa funkcijom, i set dva state-a

--- hooks/src/App.js
+++ hooks/src/App.js
@@ -1,16 +1,23 @@
 import { useState } from "react";
 function App() {
   const [counter, setCounter] = useState(0);
+  const [random, setRandom] = useState(() => {
+    return Math.random();
+  });
+  const updateCounter = () => {
+    // let a = counter+1;
+    setCounter(2);
+    setRandom(123);
+  };
 
-  // const updateCounter = ()=>{
-  //   let a = counter+1;
-  //   setCounter(a)
-  // }
-  console.log("rerendered")
-  return <div className="App">{counter}
-  <button onClick={()=>setCounter(prevState=>prevState+1)}>Count + 1</button>
-  
-  </div>;
+  return (
+    <div className="App">
+      {counter}
+      <button onClick={() => updateCounter()}>Count + 1</button> <br></br>
+      {/* <button onClick={()=>setCounter(prevState=>prevState+1)}>Count + 1</button> <br></br> */}
+      <p>Random value: {random}</p>
+    </div>
+  );
 }
 
 export default App;
