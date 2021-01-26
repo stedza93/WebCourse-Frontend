2d1153d11b134a540b6b1adbc6e9b6b1f0997f36
Stefan Miladinovic <stefan@coinis.com>
2021-01-26 06:04:03 +0100

    useEffect tri slucaja, bez drugog argumenta, drugi argument [], drugi argument [statePromjenljiva]

--- hooks/src/App.js
+++ hooks/src/App.js
@@ -1,21 +1,32 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 function App() {
   const [counter, setCounter] = useState(0);
   const [random, setRandom] = useState(() => {
     return Math.random();
   });
-  const updateCounter = () => {
-    // let a = counter+1;
-    setCounter(2);
-    setRandom(123);
-  };
+  useEffect(()=>{
+    console.log("izvrsena useEffect fja")
+  })
+  useEffect(()=>{
+    console.log("Izvrsena useEffect sa []") 
+  },[])
+
+  useEffect(()=>{
+    console.log("Izvrsena useEffect sa [random]") 
+  },[random])
+  // const updateCounter = () => {
+  //   // let a = counter+1;
+  //   setCounter(2);
+  //   setRandom(123);
+  // };
 
   return (
     <div className="App">
       {counter}
-      <button onClick={() => updateCounter()}>Count + 1</button> <br></br>
-      {/* <button onClick={()=>setCounter(prevState=>prevState+1)}>Count + 1</button> <br></br> */}
+      {/* <button onClick={() => updateCounter()}>Count + 1</button> <br></br> */}
+      <button onClick={()=>setCounter(prevState=>prevState+1)}>Count + 1</button> <br></br>
       <p>Random value: {random}</p>
+      <button onClick={()=>setRandom(Math.random())}>Randomize</button> <br></br>
     </div>
   );
 }
