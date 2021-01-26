bde366e4c0e9da4b8195a5b5dd8364bc902e81ef
Stefan Miladinovic <stefan@coinis.com>
2021-01-26 06:28:22 +0100

    useEffect cleanup funkcije

--- hooks/src/App.js
+++ hooks/src/App.js
@@ -1,19 +1,34 @@
 import { useEffect, useState } from "react";
 function App() {
   const [counter, setCounter] = useState(0);
+  const [search, setSearch] = useState("");
   const [random, setRandom] = useState(() => {
     return Math.random();
   });
-  useEffect(()=>{
-    console.log("izvrsena useEffect fja")
-  })
-  useEffect(()=>{
-    console.log("Izvrsena useEffect sa []") 
-  },[])
+  // useEffect(()=>{
+  //   console.log("izvrsena useEffect fja")
+  // })
+  // useEffect(()=>{
+  //   console.log("Izvrsena useEffect sa []")
+  // },[])
 
-  useEffect(()=>{
-    console.log("Izvrsena useEffect sa [random]") 
-  },[random])
+  // useEffect(()=>{
+  //   console.log("Izvrsena useEffect sa [random]");
+  //   return ()=>{
+  //     console.log(random, "izvrsena cleanUp f-ja")
+  //   }
+  // },[random])
+
+  useEffect(() => {
+    const intervalId = setTimeout(() => search && searchApi(), 500);
+    const searchApi = () => {
+      console.log("searching...");
+    };
+    return () => {
+      console.log(intervalId);
+      clearInterval(intervalId);
+    };
+  }, [search]);
   // const updateCounter = () => {
   //   // let a = counter+1;
   //   setCounter(2);
@@ -24,9 +39,20 @@ function App() {
     <div className="App">
       {counter}
       {/* <button onClick={() => updateCounter()}>Count + 1</button> <br></br> */}
-      <button onClick={()=>setCounter(prevState=>prevState+1)}>Count + 1</button> <br></br>
+      <button onClick={() => setCounter((prevState) => prevState + 1)}>
+        Count + 1
+      </button>{" "}
+      <br></br>
       <p>Random value: {random}</p>
-      <button onClick={()=>setRandom(Math.random())}>Randomize</button> <br></br>
+      <button onClick={() => setRandom(Math.random())}>Randomize</button>{" "}
+      <br></br>
+      <label htmlFor="search">Search</label> <br></br>
+      <input
+        type="text"
+        id="search"
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+      />
     </div>
   );
 }
