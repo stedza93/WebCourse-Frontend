063880c052ab752ea79f50e041966f8adc6e91b7
Stefan Miladinovic <you@example.com>
2021-02-26 12:05:28 +0100

    strapi project and react app

---
+++ blog-api/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'd5adbef397d3307c26f527cef46fc995'),
+    },
+  },
+});
---
+++ blog-api/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'a4c6973d-4711-4cbe-a327-a7a1e8f5eb7c'
+};
